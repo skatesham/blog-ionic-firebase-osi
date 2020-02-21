0f45d214c197dbecc4bbbe2ec931986a00d4632a
Sham Fiorin <sham.vinicius@gmail.com>
2020-02-21 00:26:15 -0300

    Readme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import * as firebase from 'firebase/app';
 
+import {FirebaseConfig} from '../../firebase-config';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -12,16 +14,7 @@ import * as firebase from 'firebase/app';
 })
 export class AppComponent {
 
-  firebaseConfig = {
-    apiKey: "AIzaSyDeZ1zpMUNu0MgJVytN-4Lv1MKkHKWWJc0",
-    authDomain: "blog-osi.firebaseapp.com",
-    databaseURL: "https://blog-osi.firebaseio.com",
-    projectId: "blog-osi",
-    storageBucket: "blog-osi.appspot.com",
-    messagingSenderId: "194832492154",
-    appId: "1:194832492154:web:c11b7542a1108465363dc3",
-    measurementId: "G-0JV2M8JLE5"
-  };
+  firebaseConfig = FirebaseConfig;
 
   constructor(
     private platform: Platform,
